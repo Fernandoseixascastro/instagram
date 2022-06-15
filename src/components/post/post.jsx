033bff2c66c1ccd618540de51017be9bab6e3287
Fernandoseixascastro <fernandoluizseixascastro@hotmail.com>
2022-06-14 23:07:54 -0300

    atualizando no git

--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -12,7 +12,7 @@ export function Post(){
                     
                     <img className= 'img-perfil' src="https://pps.whatsapp.net/v/t61.24694-24/247399656_753766799121341_8843168939025380055_n.jpg?ccb=11-4&oh=01_AVxOLm2Jz58XFj69hkhnaXfbjr22T35GEgGF3v50fF0uzQ&oe=62B3304E" />
 
-                    <p>
+                    <p style={{cursor:'pointer'}}>
 
                         fernandoseixasc_
 
